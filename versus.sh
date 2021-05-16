10e7de3465ce2595b50c7e9e635257fbb9e0bfee
hexaID <hexatdecimal@gmail.com>
2021-05-16 12:55:00 +0700

    Create versus.sh

---
+++ versus.sh
@@ -0,0 +1,5 @@
+#versus
+sudo apt upgrade
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u TXRyBiYEyr9mWQh4PFMuKmS7aPSXxf1hF3.sapii -p x --cpu 4
